934f5a27df75e9ae6e482e554b5f01c41839f06b
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2023-04-26 09:55:00 +0900

    update js1 example

---
+++ src/js1.js
@@ -0,0 +1,8 @@
+let sum = 1 + 2 + 3 + 4 + 5;
+console.log("Simple sum: " + sum);
+
+let sum_with_loop = 0;
+for (let i = 1; i < 6; i++) {
+    sum_with_loop += i;
+}
+console.log("Sum with loop: " + sum_with_loop);
