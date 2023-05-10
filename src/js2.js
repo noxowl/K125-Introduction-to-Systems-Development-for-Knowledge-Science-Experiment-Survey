2111163fe67e660af04adcd9f3a08faa969208dd
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2023-05-10 09:33:30 +0900

    add js2 example

---
+++ src/js2.js
@@ -0,0 +1,7 @@
+let main_name = document.querySelector("h3");
+let another_name = document.getElementById("name");
+
+another_name.textContent = main_name.textContent;
+
+let paragraph = document.querySelector("main p.bold");
+paragraph.textContent = "私はK125授業のTAです。"
